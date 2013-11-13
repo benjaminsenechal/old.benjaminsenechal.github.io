88f487c4486a5d50af1a4a8841da3a8ecc9dd637
benjaminsenechal <benjamin.senechal1@epsi.fr>
2013-11-13 19:25:03 +0100

    Bootstrap middleman app and develop pages

---
+++ config.rb
@@ -0,0 +1,19 @@
+activate :livereload
+activate :gzip
+activate :syntax
+activate :directory_indexes
+
+set :markdown, fenced_code_blocks: true
+set :markdown_engine, :redcarpet
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
+
+activate :deploy do |deploy|
+  deploy.method       = :git
+  deploy.branch       = 'master'
+  deploy.build_before = true
+end
+
